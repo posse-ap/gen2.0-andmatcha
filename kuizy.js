26c202d506bc3afaa1990ec82995f975cf0aad0a
Jin Aoyagi <contact.jin02@gmail.com>
2021-07-18 23:13:31 +0900

    問題の表示をループ処理化、変数名をわかりやすく変更、ナンバリングを0始めに変更

--- kuizy.js
+++ kuizy.js
@@ -1,32 +1,71 @@
+//問題番号、選択肢番号は０から数えています
+let quizNum;
+let optionNum;
+let correctNum;
+let correctNum_array = [0, 1, 2];
+let correctText_array = [
+    ['たかなわ', 'こうわ', 'たかわ'],
+    ['かめど', 'かめいど', 'かめと'],
+    ['かゆまち', 'こうじまち', 'おかとまち']];
+const quizLength = 3;
+
+//クイズタイトル、画像、選択肢、回答ボックスの表示
+for (let i = 0; i < quizLength; i++) {
+    /**クイズタイトルと画像、選択肢を含ませるdiv要素 */
+    let quizDiv = document.createElement('div');
+    quizDiv.id = `quiz-div${i}`;
+    document.body.appendChild(quizDiv);
+
+    let quizTitle = document.createElement('h2');
+    quizTitle.innerHTML = `<span class="border-underline">${i + 1}.この地名はなんて読む？</span>`;
+    quizDiv.appendChild(quizTitle);
+
+    let placenameImg = document.createElement('img');
+    placenameImg.src = `./img/photo${i}.png`;
+    quizDiv.appendChild(placenameImg);
+
+    let options = document.createElement('ul');
+    options.id = `options-quiz${i}`;
+    quizDiv.appendChild(options);
+    options.innerHTML = `<li id="option${i}-0" onclick="clickfunction(${i},0,${correctNum_array[i]})">${correctText_array[i][0]}</li>
+    <li id="option${i}-1" onclick="clickfunction(${i},1,${correctNum_array[i]})">${correctText_array[i][1]}</li>
+    <li id="option${i}-2" onclick="clickfunction(${i},2,${correctNum_array[i]})">${correctText_array[i][2]}</li>`;
+
+    let answerBoxDiv = document.createElement('div');
+    answerBoxDiv.id = `answerbox-div${i}`;
+    document.body.appendChild(answerBoxDiv);
+}
+
 //クリックした時の挙動
-var clickfunction = function (questionNo, choicesNo, correct) {　//クリックすると3つの変数が更新される
-    let clickedchoice = document.getElementById(`choice${questionNo}-${choicesNo}`);　//クリックしたliを取得
-    let correctAnswer = document.getElementById(`choice${questionNo}-${correct}`); //正解のliを取得
-    let answerBox = document.getElementById(`answer-box-${questionNo}`);　//回答ボックスのdivを取得
-    
+let clickfunction = function (quizNum, optionNum, correctNum) {　//クリックすると3つの変数が更新される
+    let clickedchoice = document.getElementById(`option${quizNum}-${optionNum}`);　//クリックしたliを取得
+    let correctAnswer = document.getElementById(`option${quizNum}-${correctNum}`); //正解のliを取得
+    let answerBoxDiv = document.getElementById(`answerbox-div${quizNum}`);　//回答ボックスのdivを取得
+    let optionsLength = document.getElementById(`options-quiz${quizNum}`).childElementCount; //該当設問における選択肢の個数を取得（基本3つ）
+
     //回答ボックスに入れる要素を作成
     let result = document.createElement('h3');
     let resultParagraph = document.createElement('p');
-    
+
     //回答済の全ての問題の回答ボックスへの表示を行う
-    let answerBox_array = [answerBox]
-    answerBox_array.forEach(answerBox => {
+    let answerBox_array = [answerBoxDiv]
+    answerBox_array.forEach(answerBoxDiv => {
         //スタイルの追加
-        answerBox.classList.add('answer-box');
+        answerBoxDiv.classList.add('answer-box');
         result.classList.add('quiz-result-title');
         resultParagraph.classList.add('quiz-result-paragraph');
         //正解を表す文面をpタグに追加
         resultParagraph.innerHTML = `正解は「${correctAnswer.innerText}」です！`;
         //divの子要素として追加することで、実際に表示する
-        answerBox.appendChild(result);
-        answerBox.appendChild(resultParagraph);
+        answerBoxDiv.appendChild(result);
+        answerBoxDiv.appendChild(resultParagraph);
     });
-    
+
     clickedchoice.classList.add('clickedchoice-red'); //クリックしたliを赤くする
     correctAnswer.classList.add('correctchoice-blue'); //正解のliを青くする（正解を選んだ場合は上書きされる）
 
     //正誤判定
-    if (correct === choicesNo) {
+    if (correctNum === optionNum) {
         result.innerHTML = '<span class="quiz-result-correct">正解！</span>'; //「正解！」を回答ボックスdiv内のh3タグに追加、スタイルも同時に追加
         console.log('正解を選択しました');
     } else {
@@ -35,10 +74,10 @@ var clickfunction = function (questionNo, choicesNo, correct) {　//クリック
     }
 
     //クリックできなくする
-    var choicesLength = document.getElementById(`choices-q${questionNo}`).childElementCount; //該当設問における選択肢の個数を取得（基本3つ）
-    for (let i = 0; i < choicesLength; i++) {
-        document.getElementById(`choice${questionNo}-${i}`).classList.add('cannotclick');
+    for (let k = 0; k < optionsLength; k++) {
+        document.getElementById(`option${quizNum}-${k}`).classList.add('cannotclick');
     };
 
-    console.log(`第${questionNo}問　この設問の選択肢は${choicesLength}つあります\n選択:${choicesNo} 正解:${correct}`);
+    //ログ表示
+    console.log(`第${quizNum}問　この設問の選択肢は${optionsLength}つあります\n選択:${optionNum} 正解:${correctNum}`);
 }
