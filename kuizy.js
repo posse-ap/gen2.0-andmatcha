8e5818070023a1f99ff8a725a1cfd161b5f857ac
Jin Aoyagi <contact.jin02@gmail.com>
2021-07-28 18:07:23 +0900

    コードを見やすくした

--- kuizy.js
+++ kuizy.js
@@ -14,28 +14,27 @@ const optionsTextArray = [
 ];
 const QUIZ_LENGTH = optionsTextArray.length;
 const CORRECT_NUM = 0;
-const container = document.getElementById('container');
+const container = document.getElementById('quiz-container');
 
 //////////表示に関する部分////////////////////
 for (let quizIndex = 0; quizIndex < QUIZ_LENGTH; quizIndex++) {
-    let optionsLength = optionsTextArray[quizIndex].length;
+    const optionsLength = optionsTextArray[quizIndex].length;
     /**クイズタイトルと画像、選択肢を含ませるdiv要素 */
-    let quizDiv = document.createElement('div');
+    const quizDiv = document.createElement('div');
     quizDiv.id = `quiz-div${quizIndex}`;
     quizDiv.classList.add('quiz-box');
     container.appendChild(quizDiv);
     //クイズタイトルの表示
-    let quizTitle = document.createElement('h2');
+    const quizTitle = document.createElement('h2');
     quizTitle.innerText = `${quizIndex + 1}.この地名はなんて読む？`;
     quizTitle.classList.add('quiz-title');
-
     quizDiv.appendChild(quizTitle);
     //画像の表示
-    let placenameImg = document.createElement('img');
-    placenameImg.src = `./img/photo${quizIndex}.png`;
+    const placenameImg = document.createElement('img');
+    placenameImg.src = `img/photo${quizIndex}.png`;
     quizDiv.appendChild(placenameImg);
     //選択肢のul作成
-    let optionsList = document.createElement('ul');
+    const optionsList = document.createElement('ul');
     optionsList.id = `options-quiz${quizIndex}`;
     optionsList.classList.add('options-list');
     quizDiv.appendChild(optionsList);
@@ -50,20 +49,20 @@ for (let quizIndex = 0; quizIndex < QUIZ_LENGTH; quizIndex++) {
         optionsList.insertAdjacentHTML('beforeend', `<li id="option${quizIndex}-${shuffleArray[optionIndex]}" class="option-item" onclick="clickfunction(${quizIndex},${shuffleArray[optionIndex]})">${optionsTextArray[quizIndex][shuffleArray[optionIndex]]}`);
     }
     //回答ボックスの表示
-    let resultDiv = document.createElement('div');
+    const resultDiv = document.createElement('div');
     resultDiv.id = `answerbox-div${quizIndex}`;
     container.appendChild(resultDiv);
 }
 
 //////////クリックした時実行する関数////////////////////
-let clickfunction = function (quizNum, chosenOptionNum) {　//クリックすると3つの変数が更新される
-    let clickedOption = document.getElementById(`option${quizNum}-${chosenOptionNum}`);　//クリックしたliを取得
-    let correctOption = document.getElementById(`option${quizNum}-${CORRECT_NUM}`); //正解のliを取得
-    let resultDiv = document.getElementById(`answerbox-div${quizNum}`);　//回答ボックスのdivを取得
-    let optionsLength = document.getElementById(`options-quiz${quizNum}`).childElementCount; //該当設問における選択肢の個数を取得（基本3つ）
+const clickfunction = function (quizNum, chosenOptionNum) {
+    const clickedOption = document.getElementById(`option${quizNum}-${chosenOptionNum}`);　//クリックしたliを取得
+    const correctOption = document.getElementById(`option${quizNum}-${CORRECT_NUM}`); //正解のliを取得
+    const resultDiv = document.getElementById(`answerbox-div${quizNum}`);　//回答ボックスのdivを取得
+    const optionsLength = document.getElementById(`options-quiz${quizNum}`).childElementCount; //該当設問における選択肢の個数を取得（基本3つ）
     //回答ボックスに入れる要素を作成
-    let result = document.createElement('h3');
-    let resultParagraph = document.createElement('p');
+    const result = document.createElement('h3');
+    const resultParagraph = document.createElement('p');
     //スタイルの追加
     resultDiv.classList.add('result-box');
     result.classList.add('result-title');
@@ -73,19 +72,19 @@ let clickfunction = function (quizNum, chosenOptionNum) {　//クリックする
     //divの子要素として追加することで、実際に表示する
     resultDiv.appendChild(result);
     resultDiv.appendChild(resultParagraph);
-    //選択肢のスタイル変更
-    clickedOption.classList.add('clicked-option'); //クリックしたliを赤くする
-    correctOption.classList.add('correct-option'); //正解のliを青くする（正解を選んだ場合は上書きされる）
+    //選択肢にスタイルを追加
+    clickedOption.classList.add('clicked-option');
+    correctOption.classList.add('correct-option');
     //正誤判定
     if (CORRECT_NUM === chosenOptionNum) {
-        result.innerHTML = '<span class="result-correct">正解！</span>';
+        result.innerHTML = '正解！';
+        result.style.borderBottom = 'solid 3px #287dff';
     } else {
-        result.innerHTML = '<span class="result-incorrect">不正解！</span>';
+        result.innerHTML = '不正解！';
+        result.style.borderBottom = 'solid 3px #ff5128';
     }
     //クリックできなくする
     for (let optionIndex = 0; optionIndex < optionsLength; optionIndex++) {
         document.getElementById(`option${quizNum}-${optionIndex}`).classList.add('cannot-click');
     };
 }
-
-//test
