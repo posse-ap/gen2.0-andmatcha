b2ccf9de35e6a0438afaf88e2e8878e45c7782f6
Jin Aoyagi <contact.jin02@gmail.com>
2021-11-30 21:40:19 +0900

    1130縦もく

--- chart-setting.js
+++ chart-setting.js
@@ -1,8 +1,8 @@
-const barCtx = document.getElementById('barChart');
-const langsCtx = document.getElementById('langagesPieChart');
-const contentsCtx = document.getElementById('contentsPieChart');
+var barCtx = document.getElementById('barChart');
+var langsCtx = document.getElementById('languagesChart');
+var contentsCtx = document.getElementById('contentsChart');
 
-const barChart = new Chart(barCtx, {
+var barChart = new Chart(barCtx, {
     type: 'bar',
     data: {
         labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31],
@@ -12,4 +12,28 @@ const barChart = new Chart(barCtx, {
     }],
     },
     options: {}
+});
+
+var langsChart = new Chart(langsCtx, {
+    type: 'doughnut',
+    data: {
+        labels: ['JavaScript', 'CSS', 'PHP', 'HTML', 'Laravel', 'SQL', 'SHEEL', '情報システム基礎知識(その他)'],
+        dataets: [{
+            data: [38, 31, 21, 10, 4, 4, 4, 4],
+            backgroundColor: [
+                '#0445EC',
+                '#0F70BD',
+                '#20BDDE',
+                '#3CCEFE',
+                '#B29EF3',
+                '#6C46EB',
+                '#4A17EF',
+                '#3005C0',
+            ],
+        }]
+    },
+    options: {
+        legend: false,
+        responsive: false,
+    }
 });
