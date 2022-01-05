52f05f3cb7d0bfca2adef83cd519cea18e147513
Jin Aoyagi <contact.jin02@gmail.com>
2022-01-05 10:26:38 +0900

    データベースからhtmlに出力

--- www/html/index.php
+++ www/html/index.php
@@ -8,7 +8,12 @@ try {
 
     $db = new PDO($dsn, $user, $password);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo '成功';
+?>
+
+<p><a href="./quiz.php?id=1">1.東京の難読地名クイズ</a></p>
+<p><a href="./quiz.php?id=2">2.広島県の難読地名クイズ</a></p>
+
+<?php
 } catch (PDOException $e) {
 
     echo '接続失敗' . $e->getMessage();
