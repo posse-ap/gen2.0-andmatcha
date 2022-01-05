e5e332bbf480add9a088352e43ce720a729d9dfc
Jin Aoyagi <contact.jin02@gmail.com>
2022-01-05 10:25:40 +0900

    画像のファイル名及びテーブル設計を一部変更

--- mysql/sql/init.sql
+++ mysql/sql/init.sql
@@ -14,42 +14,113 @@ CREATE TABLE big_questions (
 INSERT INTO
     big_questions (`name`)
 VALUES
-    ("東京の難読地名クイズ"),
-    ("広島県の難読地名クイズ");
+    ('ガチで東京の人しか解けない！#東京の難読地名クイズ'),
+    ('広島県民なら解けて当然？ #広島県の難読地名クイズ');
 
 DROP TABLE IF EXISTS questions;
 
 CREATE TABLE questions (
     `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `big_question_id` INT NOT NULL,
+    `question_id` INT NOT NULL,
     `image` VARCHAR(225) NOT NULL
 );
 
 INSERT INTO
-    questions (`big_question_id`, `image`)
+    questions (`big_question_id`, `question_id`, `image`)
 VALUES
-    (1, "takanawa.png"),
-    (1, "kameido.png"),
-    (2, "mukainada.png");
+    (1, 1, 'tokyo1.png'),
+    (1, 2, 'tokyo2.png'),
+    (1, 3, 'tokyo3.png'),
+    (1, 4, 'tokyo4.png'),
+    (1, 5, 'tokyo5.png'),
+    (1, 6, 'tokyo6.png'),
+    (1, 7, 'tokyo7.png'),
+    (1, 8, 'tokyo8.png'),
+    (1, 9, 'tokyo9.png'),
+    (1, 10, 'tokyo10.png'),
+    (2, 1, 'hiroshima1.png'),
+    (2, 2, 'hiroshima2.png'),
+    (2, 3, 'hiroshima3.png'),
+    (2, 4, 'hiroshima4.png'),
+    (2, 5, 'hiroshima5.png'),
+    (2, 6, 'hiroshima6.png'),
+    (2, 7, 'hiroshima7.png'),
+    (2, 8, 'hiroshima8.png'),
+    (2, 9, 'hiroshima9.png'),
+    (2, 10, 'hiroshima10.png');
 
 DROP TABLE IF EXISTS choices;
 
 CREATE TABLE choices (
     `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `big_question_id` INT NOT NULL,
     `question_id` INT NOT NULL,
+    `choice_id` INT NOT NULL,
     `name` VARCHAR(225) NOT NULL,
     `valid` BOOL NOT NULL
 );
 
 INSERT INTO
-    choices (`question_id`, `name`, `valid`)
+    choices (`big_question_id`, `question_id`, `choice_id`, `name`, `valid`)
 VALUES
-    (1, "たかなわ", 1),
-    (1, "たかわ", 0),
-    (1, "こうわ", 0),
-    (2, "かめと", 0),
-    (2, "かめど", 0),
-    (2, "かめいど", 1),
-    (3, "むこうひら", 0),
-    (3, "むきひら", 0),
-    (3, "むかいなだ", 1);
+    (1, 1, 1 'たかなわ', 1),
+    (1, 1, 2, 'たかわ', 0),
+    (1, 1, 3, 'こうわ', 0),
+    (1, 2, 1, 'かめいど', 1),
+    (1, 2, 2, 'かめと', 0),
+    (1, 2, 3, 'かめど', 0),
+    (1, 3, 1, 'こうじまち', 1),
+    (1, 3, 2, 'かゆまち', 0),
+    (1, 3, 3, 'おかとまち', 0),
+    (1, 4, 1, 'おなりもん', 1),
+    (1, 4, 2, 'おかどもん', 0),
+    (1, 4, 3, 'ごせいもん', 0),
+    (1, 5, 1, 'とどろき', 1),
+    (1, 5, 2, 'たたりき', 0),
+    (1, 5, 3, 'たたら', 0),
+    (1, 6, 1, 'しゃくじい', 1),
+    (1, 6, 2, 'せきこうい', 0),
+    (1, 6, 3, 'いじい', 0),
+    (1, 7, 1, 'ぞうしき', 1),
+    (1, 7, 2, 'ざっしょく', 0),
+    (1, 7, 3, 'ざっしき', 0),
+    (1, 8, 1, 'おかちまち', 1),
+    (1, 8, 2, 'みとちょう', 0),
+    (1, 8, 3, 'ごしろちょう', 0),
+    (1, 9, 1, 'ししぼね', 1),
+    (1, 9, 2, 'ろっこつ', 0),
+    (1, 9, 3, 'しこね', 0),
+    (1, 10, 1, 'こぐれ', 1),
+    (1, 10, 2, 'こばく', 0),
+    (1, 10, 3, 'こしゃく', 0),
+    (2, 1, 1, 'むかいなだ', 1),
+    (2, 1, 2, 'むこうひら', 0),
+    (2, 1, 3, 'むきひら', 0),
+    (2, 2, 1, 'みつぎ', 1),
+    (2, 2, 2, 'みよし', 0),
+    (2, 2, 3, 'おしらべ', 0),
+    (2, 3, 1, 'かなやま', 1),
+    (2, 3, 2, 'ぎんざん', 0),
+    (2, 3, 3, 'きやま', 0),
+    (2, 4, 1, 'とよひ', 1),
+    (2, 4, 2, 'としか', 0),
+    (2, 4, 3, 'とよか', 0),
+    (2, 5, 1, 'いしぐろ', 1),
+    (2, 5, 2, 'しゃくぜ', 0),
+    (2, 5, 3, 'いしあぜ', 0),
+    (2, 6, 1, 'みよし', 1),
+    (2, 6, 2, 'みつぎ', 0),
+    (2, 6, 3, 'みかた', 0),
+    (2, 7, 1, 'うずい', 1),
+    (2, 7, 2, 'くもおり', 0),
+    (2, 7, 3, 'もみち', 0),
+    (2, 8, 1, 'すもも', 1),
+    (2, 8, 2, 'でこぽん', 0),
+    (2, 8, 3, 'ぽんかん', 0),
+    (2, 9, 1, 'おおちごとうげ', 1),
+    (2, 9, 2, 'おおちごえとうげ', 0),
+    (2, 9, 3, 'おうちごとうげ', 0),
+    (2, 10, 1, 'よおろほよばら', 1),
+    (2, 10, 2, 'てっぽよばら', 0),
+    (2, 10, 3, 'ていぼよはら', 0);
--- www/html/quiz.js
+++ www/html/quiz.js
@@ -17,42 +17,42 @@ const CORRECT_NUM = 0;
 const container = document.getElementById('quiz-container');
 
 //////////表示に関する部分////////////////////
-for (let quizIndex = 0; quizIndex < QUIZ_LENGTH; quizIndex++) {
-    const optionsLength = optionsTextArray[quizIndex].length;
-    /**クイズタイトルと画像、選択肢を含ませるdiv要素 */
-    const quizDiv = document.createElement('div');
-    quizDiv.id = `quiz-div${quizIndex}`;
-    quizDiv.classList.add('quiz-box');
-    container.appendChild(quizDiv);
-    //クイズタイトルの表示
-    const quizTitle = document.createElement('h2');
-    quizTitle.innerText = `${quizIndex + 1}.この地名はなんて読む？`;
-    quizTitle.classList.add('quiz-title');
-    quizDiv.appendChild(quizTitle);
-    //画像の表示
-    const placenameImg = document.createElement('img');
-    placenameImg.src = `img/photo${quizIndex}.png`;
-    quizDiv.appendChild(placenameImg);
-    //選択肢のul作成
-    const optionsList = document.createElement('ul');
-    optionsList.id = `options-quiz${quizIndex}`;
-    optionsList.classList.add('options-list');
-    quizDiv.appendChild(optionsList);
-    //ランダムな配列を生成
-    const shuffleArray = Array.from(new Array(optionsLength)).map((v, k) => k);
-    for (let i = shuffleArray.length; 1 < i; i--) {
-        j = Math.floor(Math.random() * i);
-        [shuffleArray[j], shuffleArray[i - 1]] = [shuffleArray[i - 1], shuffleArray[j]];
-    }
-    //選択肢の表示
-    for (let optionIndex = 0; optionIndex < optionsLength; optionIndex++) {
-        optionsList.insertAdjacentHTML('beforeend', `<li id="option${quizIndex}-${shuffleArray[optionIndex]}" class="option-item" onclick="clickfunction(${quizIndex},${shuffleArray[optionIndex]})">${optionsTextArray[quizIndex][shuffleArray[optionIndex]]}`);
-    }
-    //回答ボックスの表示
-    const resultDiv = document.createElement('div');
-    resultDiv.id = `answerbox-div${quizIndex}`;
-    container.appendChild(resultDiv);
-}
+// for (let quizIndex = 0; quizIndex < QUIZ_LENGTH; quizIndex++) {
+//     const optionsLength = optionsTextArray[quizIndex].length;
+//     /**クイズタイトルと画像、選択肢を含ませるdiv要素 */
+//     const quizDiv = document.createElement('div');
+//     quizDiv.id = `quiz-div${quizIndex}`;
+//     quizDiv.classList.add('quiz-box');
+//     container.appendChild(quizDiv);
+//     //クイズタイトルの表示
+//     const quizTitle = document.createElement('h2');
+//     quizTitle.innerText = `${quizIndex + 1}.この地名はなんて読む？`;
+//     quizTitle.classList.add('quiz-title');
+//     quizDiv.appendChild(quizTitle);
+//     //画像の表示
+//     const placenameImg = document.createElement('img');
+//     placenameImg.src = `img/photo${quizIndex}.png`;
+//     quizDiv.appendChild(placenameImg);
+//     //選択肢のul作成
+//     const optionsList = document.createElement('ul');
+//     optionsList.id = `options-quiz${quizIndex}`;
+//     optionsList.classList.add('options-list');
+//     quizDiv.appendChild(optionsList);
+//     //ランダムな配列を生成
+//     const shuffleArray = Array.from(new Array(optionsLength)).map((v, k) => k);
+//     for (let i = shuffleArray.length; 1 < i; i--) {
+//         j = Math.floor(Math.random() * i);
+//         [shuffleArray[j], shuffleArray[i - 1]] = [shuffleArray[i - 1], shuffleArray[j]];
+//     }
+//     //選択肢の表示
+//     for (let optionIndex = 0; optionIndex < optionsLength; optionIndex++) {
+//         optionsList.insertAdjacentHTML('beforeend', `<li id="option${quizIndex}-${shuffleArray[optionIndex]}" class="option-item" onclick="clickfunction(${quizIndex},${shuffleArray[optionIndex]})">${optionsTextArray[quizIndex][shuffleArray[optionIndex]]}`);
+//     }
+//     //回答ボックスの表示
+//     const resultDiv = document.createElement('div');
+//     resultDiv.id = `answerbox-div${quizIndex}`;
+//     container.appendChild(resultDiv);
+// }
 
 //////////クリックした時実行する関数////////////////////
 const clickfunction = function (quizNum, chosenOptionNum) {
@@ -88,5 +88,3 @@ const clickfunction = function (quizNum, chosenOptionNum) {
         document.getElementById(`option${quizNum}-${optionIndex}`).classList.add('cannot-click');
     };
 }
-
-//test
