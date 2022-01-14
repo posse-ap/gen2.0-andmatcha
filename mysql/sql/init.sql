0be979303e5130492b4dc7241235dc85b2de129d
Jin Aoyagi <contact.jin02@gmail.com>
2022-01-14 20:49:50 +0900

    テーブル作成、ダミーデータ追加

---
+++ mysql/sql/init.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS webapp;
+
+CREATE DATABASE webapp;
+
+USE webapp;
+
+DROP TABLE IF EXISTS ;
+
+CREATE TABLE study_records (
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `date` DATE NOT NULL ,
+    `hours` INT NOT NULL UNSIGNED,
+    `lang` VARCHAR(225) NOT NULL,
+    `content` VARCHAR(225) NOT NULL
+);
+
+INSERT INTO
+    big_questions (`date`, `hours`, `lang`, `content`)
+VALUES
+    ('2022/01/14', 10, 'SQL', `POSSE課題`);
