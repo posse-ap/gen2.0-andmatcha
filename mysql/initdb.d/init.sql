efd36ecceb885390168d7e5d85b49335dbedd784
Jin Aoyagi <contact.jin02@gmail.com>
2022-01-25 20:42:06 +0900

    db初期化sqlを作成

---
+++ mysql/initdb.d/init.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS webapp;
+
+CREATE DATABASE webapp;
+
+USE webapp;
+
+DROP TABLE IF EXISTS study_records;
+
+CREATE TABLE study_records (
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `date` DATE NOT NULL,
+    `hours` INT NOT NULL,
+    `lang` VARCHAR(225) NOT NULL,
+    `content` VARCHAR(225) NOT NULL
+);
+
+INSERT INTO
+    study_records (`date`, `hours`, `lang`, `content`)
+VALUES
+    ('2022/01/14', 10, 'SQL', 'POSSE課題');
