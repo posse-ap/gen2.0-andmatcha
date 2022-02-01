6fa1598b049900ec4ccf4694b9b8036627e1ae93
Jin Aoyagi <contact.jin02@gmail.com>
2022-02-01 13:31:00 +0900

    Revert "quiz.phpのコードを切り分けました"

--- www/app/_ad.php
+++
@@ -1,3 +0,0 @@
-<div class="quizy-ad-top">
-    <a href="https://kuizy.net/shiritori" class="quizy-ad-top-text">今Kuizyお絵描きしりとりで対戦相手が待っています🎨！<span class="link-here">今すぐ遊ぶ</span>！</a>
-</div>
--- www/app/_main-contents-bottom.php
+++
@@ -1,3 +0,0 @@
-<div class="contact">
-    <p>クイズに間違いを発見された方は<a href="https://www.google.com/">こちら</a>からご報告ください。</p>
-</div>
