98d7a3e9985ad1051d39c7475093c9b720b5021d
Jin Aoyagi <contact.jin02@gmail.com>
2022-02-20 18:34:12 +0900

    選択肢の並び順をflexのorderで指定するよう変更、question_idなどの値の扱いを変更

--- www/html/quiz.php
+++ www/html/quiz.php
@@ -1,31 +1,21 @@
 <?php
-
-// TODO キーワードの整理　question, quiz -> quizに統一したい
-// TODO 機能、パーツごとにファイルを切り分ける
-
-// 問題番号(quiz_number)は1始まり、選択肢番号(choice_number)は0始まり ...問題はDBのid(1始まり)を使う、選択肢は1度配列(0始まり)に直すため
-// 任意の問題数、選択肢数に対応 ...このためにはquiz_numberは必要と考えた
-
 //ページIDが設定されていなかったらindex.phpにリダイレクトする
 $page_id = filter_input(INPUT_GET, 'id');
 if (!isset($page_id)) {
     header('Location: ./index.php');
 }
 
-//PDOの設定
-$dsn = 'mysql:host=db;dbname=quiz;charset=utf8';
-$user = 'root';
-$password = 'secret';
-$pdo = new PDO($dsn, $user, $password, [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-]);
+require('./pdo.php');
 
-//big_questionsテーブルを取得
-$big_questions_stmt = $pdo->query("SELECT * FROM big_questions");
-$big_questions = $big_questions_stmt->fetchAll();
-$big_question = $big_questions[$page_id - 1];
+//big_questionsテーブルから大問タイトルを取得
+$big_question_stmt = $pdo->prepare("SELECT title FROM big_questions WHERE id = ?");
+$big_question_stmt->execute([$page_id]);
+$big_question = $big_question_stmt->fetch();
 
+//questionsテーブルの、この大問に該当するレコードを取得
+$questions_stmt = $pdo->prepare("SELECT * FROM questions WHERE big_question_id = ?");
+$questions_stmt->execute([$page_id]);
+$questions = $questions_stmt->fetchAll();
 ?>
 
 <!DOCTYPE html>
@@ -37,55 +27,48 @@ $big_question = $big_questions[$page_id - 1];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $big_question['title']; ?></title>
     <link rel="stylesheet" href="quiz.css">
-    <link rel="shortcut icon" href="img/favicon.ico">
 </head>
 
 <body>
     <div class="content-wrapper">
         <h1><?= $big_question['title']; ?></h1>
-        <!-- クイズ部分 -->
         <div class="quiz-container">
-            <?php
-            //questionsテーブルを取得
-            $questions_stmt = $pdo->prepare("SELECT * FROM questions WHERE big_question_id = ?");
-            $questions_stmt->execute([$page_id]);
-            $questions = $questions_stmt->fetchAll();
 
-            $quiz_number = 1;
-            foreach ($questions as $question) :
-                //この設問の選択肢を取得(choicesテーブル)
-                $choices_stmt = $pdo->prepare("SELECT * FROM choices WHERE big_question_id = :big_question_id AND quiz_number = :quiz_number");
-                $choices_stmt->execute(['big_question_id' => $page_id, 'quiz_number' => $quiz_number]);
+            <!-- 設問に関するループ -->
+            <?php
+            foreach ($questions as $index => $question) :
+                $question_id = $question['id']; //questionsテーブルのidを取得、設問ごとに再代入される
+
+                //choicesテーブルの、この設問に該当するレコードを取得
+                $choices_stmt = $pdo->prepare("SELECT * FROM choices WHERE question_id = ?");
+                $choices_stmt->execute([$question_id]);
                 $choices = $choices_stmt->fetchAll();
+
+                $order = range(0, count($choices) - 1); //シャッフル用: 0始まりで、長さが選択肢の数である小さい順の整数の配列
+                shuffle($order); //設問ごとにシャッフルする
             ?>
+                <!-- クイズボックス -->
                 <div class="quiz-box">
-                    <h1 class="quiz-title"><?= $quiz_number; ?>.この地名はなんて読む？</h1>
+                    <h1 class="quiz-title"><?= $index + 1; ?>.この地名はなんて読む？</h1>
                     <img src="./img/<?= $question['image']; ?>">
-                    <ul id="choices<?= $quiz_number; ?>" class="choices-list">
-                        <?php
-                        //1,2,3の配列を1問ごとにシャッフル->選択肢の順序を並び替え
-                        $new_choice_numbers = range(0, count($choices) - 1);
-                        shuffle($new_choice_numbers);
-                        $initial_choice_number = 0;
-                        foreach ($choices as $choice) :
-                            $choice_number = $new_choice_numbers[$initial_choice_number];
-                            $choice = $choices[$choice_number]; //シャッフルした後のchoiceを再代入
-                        ?>
-                            <li id="choice<?= $quiz_number; ?>_<?= $choice_number; ?>" class="choice-item" onclick="clickfunction(<?= $quiz_number; ?>,<?= $choice_number; ?>, <?= $choice['valid']; ?>)"><?= $choice['name']; ?></li>
-                        <?php
-                            $initial_choice_number++;
-                        endforeach;
-                        ?>
+                    <ul id="choices<?= $question_id; ?>" class="choices-list">
+
+                        <!-- 選択肢に関するループ -->
+                        <?php foreach ($choices as  $index => $choice) : ?>
+                            <li id="choice<?= $question_id; ?>_<?= $index + 1; ?>" class="choice-item" style="order: <?= $order[$index]; ?>;" onclick="clickfunction(<?= $question_id; ?>,<?= $index + 1; ?>, <?= $choice['valid']; ?>)">
+                                <?= $choice['name']; ?>
+                            </li>
+                        <?php endforeach; ?>
+
                     </ul>
                 </div>
-                <div id="comment_box<?= $quiz_number; ?>" class="comment-box hide">
-                    <h3 id="comment_title<?= $quiz_number; ?>" class="comment-title"></h3>
-                    <p id="comment_text<?= $quiz_number; ?>" class="comment-text"><?= $question['comment']; ?></p>
+                <!-- コメントボックス -->
+                <div id="comment_box<?= $question_id; ?>" class="comment-box hide">
+                    <h3 id="comment_title<?= $question_id; ?>" class="comment-title"></h3>
+                    <p id="comment_text<?= $question_id; ?>" class="comment-text"><?= $question['comment']; ?></p>
                 </div>
-            <?php
-                $quiz_number++;
-            endforeach;
-            ?>
+            <?php endforeach; ?>
+
         </div>
     </div>
 
