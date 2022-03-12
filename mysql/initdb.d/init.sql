e5ca9bf11d5140623a6edadbbdb00c49147ea48d
Jin Aoyagi <contact.jin02@gmail.com>
2022-03-12 23:09:13 +0900

    学習言語と学習コンテンツのマスターテーブルを作成

--- mysql/initdb.d/init.sql
+++ mysql/initdb.d/init.sql
@@ -7,14 +7,49 @@ USE webapp;
 DROP TABLE IF EXISTS study_records;
 
 CREATE TABLE study_records (
-    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `date` DATE NOT NULL,
-    `hours` INT NOT NULL,
-    `lang` VARCHAR(225) NOT NULL,
-    `content` VARCHAR(225) NOT NULL
+  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `date` DATE NOT NULL,
+  `hours` INT NOT NULL,
+  `lang_id` INT NOT NULL,
+  `content_id` INT NOT NULL
 );
 
 INSERT INTO
-    study_records (`date`, `hours`, `lang`, `content`)
+  study_records (`date`, `hours`, `lang_id`, `content_id`)
 VALUES
-    ('2022/01/14', 10, 'SQL', 'POSSE課題');
+  ('2022/01/14', 10, 6, 3);
+  ('2022/01/14', 4, 3, 3);
+  ('2022/01/25', 2, 3, 1);
+
+DROP TABLE IF EXISTS langs;
+
+CREATE TABLE langs (
+  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `name` VARCHAR(225) NOT NULL
+);
+
+INSERT INTO
+  langs (`name`)
+VALUES
+  ('JavaScript'),
+  ('CSS'),
+  ('PHP'),
+  ('HTML'),
+  ('Laravel'),
+  ('SQL'),
+  ('SHELL'),
+  ('情報システム基礎知識(その他)');
+
+DROP TABLE IF EXISTS contents;
+
+CREATE TABLE contents (
+  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `name` VARCHAR(225) NOT NULL
+);
+
+INSERT INTO
+  contents (`name`)
+VALUES
+  ('ドットインストール'),
+  ('N予備校'),
+  ('POSSE課題');
