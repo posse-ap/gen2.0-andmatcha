50e57e69f3c568d9b011814b39523011626b9643
Jin Aoyagi <contact.jin02@gmail.com>
2022-03-20 21:14:50 +0900

    ファイル切り出しに伴う変更、その他

--- www/html/index.php
+++ www/html/index.php
@@ -1,14 +1,17 @@
 <?php
 
 try {
-    require('../app/db-settings.php');
-    require('./top-page.php');
+  require('../app/db-settings.php');
+} catch (PDOException $e) {
+
+  echo '接続失敗' . $e->getMessage();
+  exit();
+}
+
+$this_month = '202201';
+$today = '20220117';
+require('../app/query.php');
+require('./top-page.php');
 ?>
 
 <?php
-
-} catch (PDOException $e) {
-
-    echo '接続失敗' . $e->getMessage();
-    exit();
-}
