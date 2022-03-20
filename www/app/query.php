ce0989d4cef1015b1af169d6f35bb25b1f71cea4
Jin Aoyagi <contact.jin02@gmail.com>
2022-03-20 21:13:11 +0900

    データの取得部分を別ファイルに切り出し、top pageのhtml モーダル・フッター以外

---
+++ www/app/query.php
@@ -0,0 +1,101 @@
+<?php
+
+//言語一覧
+$langs_stmt = $db->query("SELECT * FROM langs");
+$langs = $langs_stmt->fetchAll();
+
+//コンテンツ一覧
+$contents_stmt = $db->query("SELECT * FROM contents");
+$contents = $contents_stmt->fetchAll();
+
+// 合計学習時間
+$total_stmt = $db->prepare("SELECT SUM(`hours`) AS total FROM study_records");
+$total_stmt->execute();
+$total = $total_stmt->fetch()['total'];
+
+// 今月の学習時間
+$this_month_stmt = $db->prepare("SELECT SUM(`hours`) AS `sum` FROM study_records WHERE DATE_FORMAT(`date`, '%Y%m') = ?");
+$this_month_stmt->execute([$this_month]);
+$this_month_sum = $this_month_stmt->fetch()['sum'];
+
+// 今日の学習時間
+$today_stmt = $db->prepare("SELECT SUM(`hours`) AS `sum` FROM study_records WHERE DATE_FORMAT(`date`, '%Y%m%d') = ?");
+$today_stmt->execute([$today]);
+$today_sum = $today_stmt->fetch()['sum'];
+
+//日毎の学習時間（棒グラフ用） 日付の連番テーブルを作成し、それに日毎の学習時間を結合(学習記録を送信していない日があっても正しく表示されるように)
+$daily_stmt = $db->prepare(
+  "WITH RECURSIVE date_table(date_value) AS (
+    SELECT
+      DATE_FORMAT(:today, '%Y-%m-01')
+    UNION ALL
+    SELECT
+      DATE_ADD(date_value, INTERVAL 1 DAY)
+    FROM
+      date_table
+    WHERE
+      date_value < LAST_DAY(:today)
+  )
+  SELECT
+    date_value, `sum`
+  FROM
+    date_table
+  LEFT JOIN
+    (SELECT `date`, SUM(`hours`) AS `sum` FROM study_records WHERE DATE_FORMAT(`date`, '%Y%m') = DATE_FORMAT(:today, '%Y%m') GROUP BY DATE_FORMAT(`date`, '%Y%m%d')) AS study_hours
+  ON
+    date_table.date_value = study_hours.date"
+);
+$daily_stmt->execute([':today' => $today]);
+$daily_sum = $daily_stmt->fetchAll();
+
+//ドーナツグラフ用 言語ごとの学習時間
+$hours_lang_stmt = $db->prepare(
+  "WITH daily_hours AS (
+    SELECT
+      study_record_id,
+      lang_id,
+      `hours`/COUNT(*) OVER(PARTITION BY study_record_id) AS hours_lang
+    FROM
+      studied_langs
+    LEFT JOIN study_records ON study_records.id = studied_langs.study_record_id
+    WHERE
+      DATE_FORMAT(`date`, '%Y%m') = DATE_FORMAT(:today, '%Y%m')
+  )
+  SELECT
+    lang_id,
+    SUM(daily_hours.hours_lang) AS sum_lang
+  FROM
+    daily_hours
+  GROUP BY
+    lang_id
+  ORDER BY
+    lang_id"
+);
+$hours_lang_stmt->execute([':today' => $today]);
+$hours_lang = $hours_lang_stmt->fetchAll();
+
+//ドーナツグラフ用 コンテンツごとの学習時間
+$hours_content_stmt = $db->prepare(
+  "WITH daily_hours AS (
+    SELECT
+      study_record_id,
+      content_id,
+      `hours`/COUNT(*) OVER(PARTITION BY study_record_id) AS hours_content
+    FROM
+      studied_contents
+    LEFT JOIN study_records ON study_records.id = studied_contents.study_record_id
+    WHERE
+      DATE_FORMAT(`date`, '%Y%m') = DATE_FORMAT(:today, '%Y%m')
+  )
+  SELECT
+    content_id,
+    SUM(hours_content) AS sum_content
+  FROM
+    daily_hours
+  GROUP BY
+    content_id
+  ORDER BY
+    content_id"
+);
+$hours_content_stmt->execute([':today' => $today]);
+$hours_content = $hours_content_stmt->fetchAll();
--- www/html/top-page.php
+++ www/html/top-page.php
@@ -1,44 +1,89 @@
 <?php
-
-$this_month = '202201';
-$today = '20220117';
-
-// 合計学習時間
-$total_stmt = $db->prepare("SELECT SUM(`hours`) AS total FROM study_records");
-$total_stmt->execute();
-$total = $total_stmt->fetch()['total'];
-
-// 今月の学習時間
-$this_month_stmt = $db->prepare("SELECT SUM(`hours`) AS `sum` FROM study_records WHERE DATE_FORMAT(`date`, '%Y%m') = ?");
-$this_month_stmt->execute([$this_month]);
-$this_month_sum = $this_month_stmt->fetch()['sum'];
-
-// 今日の学習時間
-$today_stmt = $db->prepare("SELECT SUM(`hours`) AS `sum` FROM study_records WHERE DATE_FORMAT(`date`, '%Y%m%d') = ?");
-$today_stmt->execute([$today]);
-$today_sum = $today_stmt->fetch()['sum'];
-
-//日毎の学習時間（棒グラフ用）
-$daily_stmt = $db->prepare("SELECT SUM(`hours`) AS `sum` FROM study_records WHERE DATE_FORMAT(`date`, '%Y%m') = ? GROUP BY DATE_FORMAT(`date`, '%Y%m%d')");
-$daily_stmt->execute([$this_month]);
-$daily_sum = $daily_stmt->fetchAll();
-
-printf('合計学習時間: %d時間, 今月の学習時間: %d時間, 今日の学習時間: %d時間', $total, $this_month_sum, $today_sum);
-foreach($daily_sum as $value) {
-  print_r($value['sum']);
-}
+// テストコード
+// print_r($hours_lang);
+// print_r($hours_content);
 
 ?>
 
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>POSSE Webapp</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>POSSE Webapp</title>
+  <link rel="stylesheet" href="./css/destyle.css">
+  <link rel="stylesheet" href="./css/style.css">
 </head>
+
 <body>
-    <h1>aaaaa</h1>
+  <div class="modal">
+    <div class="modal__container">
+
+    </div>
+  </div>
+
+  <header class="header">
+    <div class="header__logo">
+      <img src="./img/posse_logo.jpeg">
+    </div>
+    <p class="header__week">
+      4th week
+    </p>
+    <button class="header__button">
+      記録・投稿
+    </button>
+  </header>
+
+  <div class="main">
+    <div class="main__container main__container_v">
+      <div class="main__container__area_hours">
+        <div class="main__container__area_hours__box main__container__area_tile">
+          <p class="main__container__area_hours__box__title">Today</p>
+          <p class="main__container__area_hours__box__num"><?= $today_sum; ?></p>
+          <p class="main__container__area_hours__box__unit">hours</p>
+        </div>
+        <div class="main__container__area_hours__box main__container__area_tile">
+          <p class="main__container__area_hours__box__title">Month</p>
+          <p class="main__container__area_hours__box__num"><?= $this_month_sum; ?></p>
+          <p class="main__container__area_hours__box__unit">hours</p>
+        </div>
+        <div class="main__container__area_hours__box main__container__area_tile">
+          <p class="main__container__area_hours__box__title">Total</p>
+          <p class="main__container__area_hours__box__num"><?= $total; ?></p>
+          <p class="main__container__area_hours__box__unit">hours</p>
+        </div>
+      </div>
+      <div class="main__container__area_bar_chart main__container__area_tile"></div>
+    </div>
+    <div class="main__container">
+      <div class="main__container__area_doughnut_chart main__container__area_tile">
+        <h2>学習言語</h2>
+        <div class="main__container__area_doughnut_chart__box"></div>
+        <ul>
+          <?php foreach ($langs as $lang) : ?>
+            <li>
+              <div class="main__container__area_doughnut_chart__list_circle"></div>
+              <?= $lang['name']; ?>
+            </li>
+          <?php endforeach; ?>
+        </ul>
+      </div>
+      <div class="main__container__area_doughnut_chart main__container__area_tile">
+        <h2>学習コンテンツ</h2>
+        <div class="main__container__area_doughnut_chart__box"></div>
+        <ul>
+          <?php foreach ($contents as $content) : ?>
+            <li>
+              <div class="main__container__area_doughnut_chart__list_circle"></div>
+              <?= $content['name']; ?>
+            </li>
+          <?php endforeach; ?>
+        </ul>
+      </div>
+    </div>
+  </div>
 </body>
+
 </html>
