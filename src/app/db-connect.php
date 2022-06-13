7400cc14c7b1bc59ee8f64e2a1efa598b811a44e
andmatcha <jj.mcr185@gmail.com>
2022-06-13 16:37:19 +0900

    db接続

---
+++ src/app/db-connect.php
@@ -0,0 +1,8 @@
+<?php
+$dsn = 'mysql:host=db;dbname=webapp;charset=utf8';
+$user = 'root';
+$password = 'password';
+$db = new PDO($dsn, $user, $password, [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+]);
